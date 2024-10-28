bef7d8af2f9fc49dd93f40420eaf48e571d4bbbe
LuisVieira0701 <luisvieira@usf.edu>
2024-10-27 23:30:16 -0400

    Fixing Issues

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const ctx = canvas.getContext('2d');
 const clearCanvasButton = document.getElementById('clear-canvas')
 const colorInput = document.getElementById('choose-color')  
 
+let drawing = false
+let startX,startY
+let tool='pencil'
+let color='#000000'
+
 //Task 4- Add color selection and Canvas Clearing
 colorInput.addEventListener('input', (event) =>{
     color = event.target.value
@@ -13,21 +18,13 @@ clearCanvasButton.addEventListener('click', () => {
 })
 
 //Task 2- Configure the JavaScript for Drawing Context
-let drawing = false
-
-if (document.querySelector('input[value="pencil"]')) {
-    canvas.addEventListener('mousedown', () => {
+    canvas.addEventListener('mousedown', (event) => {
         drawing = true
+        startX=event.offsetX
+        startY=event.offsetY
         ctx.beginPath()
     });
-    
-    canvas.addEventListener('mousemove', (event) => {
-        if (drawing) {
-            ctx.lineTo(event.offsetX, event.offsetY)
-            ctx.stroke()
-        }
-    });
-    
+     
     canvas.addEventListener('mouseup', () => {
         drawing = false
         ctx.closePath()
@@ -37,33 +34,37 @@ if (document.querySelector('input[value="pencil"]')) {
         drawing=false
         ctx.closePath();
     })
-} 
-
 
 //Task3-  Implement shape drawing logic for lines, rectangles, and circles.
-if (document.querySelector('input[value="rectangle"]')) {
-    let startX, startY;
-
-    canvas.addEventListener('mousedown', (event) => {
-        drawing = true
-        startX=event.offsetX;
-        startY=event.offsetY;
+    const tools = document.querySelectorAll('input[name="tool"]');
+    tools.forEach((toolButton)=> {
+        toolButton.addEventListener('change', (event) =>{
+            tools=event.target.value
+        })
     })
 
     canvas.addEventListener('mousemove',(event)=> {
         if (drawing){
             ctx.clearRect(0,0,canvas.width,canvas.height)
-            ctx.beginPath();
-            ctx.rect(startX,startY,event.offsetX-startX,event.offsetY-startY)
-            ctx.stroke()
-        }
-    });
+            ctx.strokeStyle = color
+            ctx.beginPath()
 
-    canvas.addEventListener('mouseout',() =>{
-        drawing=false
-        ctx.closePath()
-    })
-}
+        if (tool === 'rectangle') {
+            ctx.rect(startX,startY,event.offsetX,event.offsetY);
+            ctx.stroke()
+        } else if (tool ==='pencil') {
+            ctx.moveTo(startX,startY)
+            ctx.lineTo(event.offsetX,event.offsetY)
+            ctx.stroke()
+            startX=event.offsetX
+            startY=event.offsetY
+        }
+            }
+        }
+    );
+
+    
+
 
 
 
